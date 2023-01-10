cf45f3edcd427684f30fcf7a9af78920538022b0
Leonardo do Nascimento Garcia <leonprogramacao@gmail.com>
2023-01-10 17:07:10 -0300

    Implementando método para listar pessoas

--- src/main/java/br/com/attornatus/service/PessoaService.java
+++ src/main/java/br/com/attornatus/service/PessoaService.java
@@ -1,15 +1,26 @@
 package br.com.attornatus.service;
 
+import br.com.attornatus.PessoaModel;
 import br.com.attornatus.entity.Pessoa;
+import br.com.attornatus.exception.PessoaNaoEncontradaException;
+import br.com.attornatus.mapper.PessoaMapper;
 import br.com.attornatus.repository.PessoaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PessoaService {
 
   private final PessoaRepository pessoaRepository;
+
+  private final PessoaMapper pessoaMapper;
+
+  public List<Pessoa> listar() {
+    return pessoaRepository.findAll();
+  }
 
   public Pessoa criar(Pessoa pessoa) {
     return pessoaRepository.save(pessoa);
