7ae03b02ac6859b80a3493ef6468e771c783010d
Leonardo do Nascimento Garcia <leonprogramacao@gmail.com>
2023-01-10 15:50:29 -0300

    Criação do repository

---
+++ src/main/java/br/com/attornatus/repository/PessoaRepository.java
@@ -0,0 +1,8 @@
+package br.com.attornatus.repository;
+
+import br.com.attornatus.entity.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {}
