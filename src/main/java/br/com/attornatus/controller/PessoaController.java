6393bd5f1907aa91948ebea2cf8fcce2d3776f11
Leonardo do Nascimento Garcia <leonprogramacao@gmail.com>
2023-01-10 17:06:41 -0300

    Implementando método para listar pessoas

--- src/main/java/br/com/attornatus/controller/PessoaController.java
+++ src/main/java/br/com/attornatus/controller/PessoaController.java
@@ -1,15 +1,21 @@
 package br.com.attornatus.controller;
 
+import br.com.attornatus.PessoaModel;
 import br.com.attornatus.entity.Pessoa;
 import br.com.attornatus.service.PessoaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/pessoas")
@@ -17,6 +23,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PessoaController {
 
   private final PessoaService pessoaService;
+
+  @GetMapping
+  public List<Pessoa> listar() {
+    return pessoaService.listar();
+  }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
